881c76a309fb3b3cc5f6c30320ed192d763cd274
Caua <cauapxa@gmail.com>
2026-06-12 00:57:48 -0300

    feat: Grafico de tipos de tumores com plot_class_distribution()

--- src/analysis/analysis_df.py
+++ src/analysis/analysis_df.py
@@ -13,17 +13,40 @@ O intuito do módulo analysis concentra-se exclusivamente na análise dos dados 
 """
 
 ATRIBUTOS_IMPORTANTES = [
-    "radius_mean", 
-    "area_mean", 
+    "radius_mean",
+    "texture_mean",
     "perimeter_mean",
-    "texture_mean"
+    "area_mean",
+    "smoothness_mean",
+    "compactness_mean",
+    "concavity_mean",
+    "concave_points_mean",
+    "symmetry_mean",
+    "fractal_dimension_mean",
+    "radius_se",
+    "texture_se",
+    "perimeter_se",
+    "area_se",
+    "smoothness_se",
+    "compactness_se",
+    "concavity_se",
+    "concave_points_se",
+    "symmetry_se",
+    "fractal_dimension_se",
+    "radius_worst",
+    "texture_worst",
+    "perimeter_worst",
+    "area_worst",
+    "smoothness_worst",
+    "compactness_worst",
+    "concavity_worst",
+    "concave_points_worst",
+    "symmetry_worst",
+    "fractal_dimension_worst",
 ]
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
 OUTPUT_DIR = ROOT_DIR / "docs" / "figures" 
-
-RAW_OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
-PROCESSED_OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def load_dataset(path: Path) -> pd.DataFrame:
@@ -50,11 +73,26 @@ def save_plot(grafico: plt.figure, nome_grafico: str, dataset_type: str):
     grafico.savefig(output_path / nome_grafico, dpi = 300, bbox_inches = "tight")
     plt.close(grafico)
 
+def plot_class_distribution(df: pd.DataFrame, dataset_type: str):
+    # modela o grafico
+    fig, ax = plt.subplots(figsize=(6,4))
+    sb.countplot(data=df, x="diagnosis_label", order=["Benigno", "Maligno"], ax=ax)
+    ax.set_title("Distribuição de Tumores")
+    ax.set_xlabel("Classe")
+    ax.set_ylabel("Quantidade")
+    ax.set_axisbelow(True)
+    ax.margins(y=0.10)
+    # escreve o numero que foi contado pelo countplot()
+    for container in ax.containers:
+        ax.bar_label(container, fmt="%d", padding=4)
+    # salva o grafico
+    save_plot(fig, "distribuicao_BM.png", dataset_type)
+
 """ Testando as funções """
 def main():
-    path = Path("data/raw/wdbc.data")
-    df = load_dataset(path)
-    print(df)
+    path_raw = Path("data/raw/wdbc.data")
+    df = load_dataset(path_raw)
+    plot_class_distribution(df, "raw")
 
 if __name__ == "__main__":
     main()        
