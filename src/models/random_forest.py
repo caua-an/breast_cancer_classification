5a8234ccf4dc8f1020fc43c67b38bcfecb9322fc
V1torMiranda <vitor.academico14@gmail.com>
2026-06-15 20:59:31 -0300

    feat: implementa treinamento e avaliação com Random Forest

---
+++ src/models/random_forest.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix
+)
+
+
+def main():
+
+    df = pd.read_csv("data/processed/wdbc_data_normalized.csv")
+
+    # Converter classes
+    df["diagnosis"] = df["diagnosis"].map({
+        "B": 0,
+        "M": 1
+    })
+
+    X = df.drop("diagnosis", axis=1)
+    y = df["diagnosis"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42,
+        stratify=y
+    )
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    #Treinar o modelo
+
+    model.fit(X_train, y_train)
+
+    # Fazer previsões
+
+    y_pred = model.predict(X_test)
+
+    print("\n===== RANDOM FOREST =====")
+
+    # Metricas de avaliação
+
+    print(f"Accuracy : {accuracy_score(y_test, y_pred):.4f}")
+    print(f"Precision: {precision_score(y_test, y_pred):.4f}")
+    print(f"Recall   : {recall_score(y_test, y_pred):.4f}")
+    print(f"F1-score : {f1_score(y_test, y_pred):.4f}")
+
+    print("\nConfusion Matrix")
+    print(confusion_matrix(y_test, y_pred))
+
+
+if __name__ == "__main__":
+    main()
